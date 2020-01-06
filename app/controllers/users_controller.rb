ba6dd937749583e41440e2a56370e1cde988d2bc
Mickael <mickael.spilmont@hospimedia-groupe.fr>
2020-01-06 10:32:03 +0100

    Adding possibility of login, logout, and session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
 
   def login
   end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to "/users/home"
+  end
+
+  def check
+    @current_user = User.where(name: params[:name], password: params[:password]).first
+    if @current_user
+      session[:user_id] = @current_user.id
+      flash[:info] = "You are logged in as #{@current_user.name} !"
+      redirect_to "/users/home"
+    else
+      session[:user_id] = nil
+      flash[:info] = "Connexion faild"
+      redirect_to "/users/login"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/home'
-  get 'users/login'
+  get 'users/home' => 'users#home'
+  get 'users/login' => 'users#login'
+  post 'users/login' => 'users#check'
+  get 'users/logout' => 'users#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
